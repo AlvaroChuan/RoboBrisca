e90df24b65a584cb5d55b7d77d6f8f10a7d8f96f
AlvaroChuan <alvarochuan@gmail.com>
2022-02-28 18:01:27 +0100

    notlaplayer varios turnos

--- Players/NOTLAPlayer.py
+++ Players/NOTLAPlayer.py
@@ -22,51 +22,51 @@ class NOTLAPlayer(Player):
         best_score = -math.inf
         best_action = 0
 
-        for action in list_actions:
+        for i in range(5000):
+            for action in list_actions:
 
-            new_obs = observation.clone()
-            score = self.forward_model.play(new_obs, action, self.heuristic)
-            turn_heuristic.append(score)
-            pos += 1
-            other_player = player_id + 1
-
-            while pos < 4:
-
-                if other_player == 4:
-                    other_player = 0
-
-                other_card = new_obs.hands[other_player].get_card(random.choice(range(new_obs.hands[other_player].len())))          # Selecciona una carta al azar de otro jugador
-                other_action = Action(other_card)                                                   # Transfoma la carta elegida a una acción
-                other_score = self.forward_model.play(new_obs, other_action, self.heuristic)        # Juega la carta del otro jugador
-                turn_heuristic.append(other_score)
+                new_obs = observation.clone()
+                score = self.forward_model.play(new_obs, action, self.heuristic)
+                turn_heuristic.append(score)
                 pos += 1
-                other_player += 1
+                other_player = player_id + 1
 
-            if pos_initial == 0:
-                team1 = turn_heuristic[0] + turn_heuristic[2]
-                team2 = turn_heuristic[1] + turn_heuristic[3]
+                while pos < 4:
 
-            elif pos_initial == 1:
-                team1 = turn_heuristic[0] + turn_heuristic[2]
-                team2 = turn_heuristic[1]
+                    if other_player == 4:
+                        other_player = 0
 
-            elif pos_initial == 2:
-                team1 = turn_heuristic[0]
-                team2 = turn_heuristic[1]
+                    other_card = new_obs.hands[other_player].get_card(random.choice(range(new_obs.hands[other_player].len())))          # Selecciona una carta al azar de otro jugador
+                    other_action = Action(other_card)                                                   # Transfoma la carta elegida a una acción
+                    other_score = self.forward_model.play(new_obs, other_action, self.heuristic)        # Juega la carta del otro jugador
+                    turn_heuristic.append(other_score)
+                    pos += 1
+                    other_player += 1
 
-            else:
-                team1 = turn_heuristic[0]
-                team2 = 0
+                if pos_initial == 0:
+                    team1 = turn_heuristic[0] + turn_heuristic[2]
+                    team2 = turn_heuristic[1] + turn_heuristic[3]
 
-            heuristic_difference = team1 - team2
-            final_heuristic.append(heuristic_difference)
-            turn_heuristic = []
-            pos = pos_initial
+                elif pos_initial == 1:
+                    team1 = turn_heuristic[0] + turn_heuristic[2]
+                    team2 = turn_heuristic[1]
 
-            if heuristic_difference > best_score:
-                best_score = heuristic_difference
-                best_action = action
+                elif pos_initial == 2:
+                    team1 = turn_heuristic[0]
+                    team2 = turn_heuristic[1]
 
+                else:
+                    team1 = turn_heuristic[0]
+                    team2 = 0
+
+                heuristic_difference = team1 - team2
+                final_heuristic.append(heuristic_difference)
+                turn_heuristic = []
+                pos = pos_initial
+
+                if heuristic_difference > best_score:
+                    best_score = heuristic_difference
+                    best_action = action
         return best_action
 
     def __str__(self):
